53c755f1cd1bce18a099a41f5f32e3c41c9c5862
Alex <102915735+aleferraz21@users.noreply.github.com>
2026-03-28 11:46:57 -0300

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // 1. Adicione esta linha no topo
 
 const firebaseConfig = {
-  // ... (suas chaves que já estão lá)
+ 
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app); // 2. Adicione esta linha aqui
+const db = getFirestore(app); 
 
-export { auth, db }; // 3. Exportação nomeada para o Admin e Login funcionarem
+export { auth, db }; 
 export default app;
