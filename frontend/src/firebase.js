5acfdf50f33d3020d7a9d7a04a7fb2f66c6b6c1e
Alex <102915735+aleferraz21@users.noreply.github.com>
2026-03-28 11:54:40 -0300

    Remove duplicate import of getFirestore

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,6 @@
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // 1. Adicione esta linha no topo
 
 const firebaseConfig = {
  
